1890f4544996405b67657ec4b257e8432f8f92c4
Enrique <Enrique.PazPerez@spilgames.com>
2012-07-11 09:23:52 +0200

    OPT eunit samples optimizations

--- code/tsListsEunit.erl
+++ code/tsListsEunit.erl
@@ -1,25 +1,23 @@
 simple_cases_test_() ->
-    [
-        ?_assertEqual([], ts_lists:remove_duplicates([])),
-        % Conceptually wrong testcase!!
-        ?_assertEqual([1, 2, 3, 4, 5], ts_lists:remove_duplicates([5, 1, 2, 2, 3, 4, 4]))
-    ].
-
-lists_from_file_test_() ->
-    {setup, fun() ->
-            ok=filelib:ensure_dir(?VALID_TEST_FILE),
-            ok=file:write_file(?VALID_TEST_FILE,"[1, 2, -2, -2, -1].[-2, 2, 2, 1]."),
-            file:delete(?INVALID_TEST_FILE),
-            {?VALID_TEST_FILE, ?INVALID_TEST_FILE}
-        end,
-        fun ({F, F2}) -> ok=file:delete(F) end,
-        fun ({F, F2}) -> [
-            ?_assertEqual({ok, [[1, 2, -2, -2, -1], [-2, 2, 2, 1]]}, ts_lists:load_lists_from_file(F)),
-            ?_assertEqual({error, enoent}, ts_lists:load_lists_from_file(F2)),
-            ?_test(begin
-                {ok, [L1, L2]} = ts_lists:remove_duplicates_from_lists_file(F),
-                ?assertEqual(4, length(L1)), ?assertEqual(3, length(L2))
-        end)]
-    end}.
-
+    [ ?_assertEqual([], ts_lists:remove_duplicates([])),
+        % Bad testcase (dependent / forcing on the implementation!!)
+        ?_assertEqual([1, 2, 3, 4, 5], ts_lists:remove_duplicates([1, 2, 2, 3, 4, 4, 5])) ].
+load_lists_from_file_test_() ->
+    {setup,
+        % SETUP creates a valid file with 2 lists and ensures InvalidFile does not exist
+        fun() -> setup_files() end,
+        fun ({ValidFile, _InvalidFile}) -> ok=file:delete(ValidFile) end,
+        fun ({ValidFile, InvalidFile}) ->
+            [ ?_assertEqual({ok, [[1, 2, -2, -2, -1], [-2, 2, 2, 1]]}, ts_lists:load_lists_from_file(ValidFile)),
+              ?_assertEqual({error, enoent}, ts_lists:load_lists_from_file(InvalidFile)) ]
+        end
+    }.
+remove_duplicates_from_lists_file_test_() ->
+    {setup,
+        fun() -> setup_files() end,
+        fun ({ValidFile, _InvalidFile}) -> ok=file:delete(ValidFile) end,
+        fun ({ValidFile, InvalidFile}) ->
+            [ ?_assertEqual({error, enoent}, ts_lists:remove_duplicates_from_lists_file(InvalidFile)),
+              ?_assertMatch({ok, [L1, L2]} when is_list(L1) and is_list(L2) , ts_lists:remove_duplicates_from_lists_file(ValidFile)) ]
+        end}.
 
